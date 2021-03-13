965fa6f7a649b232fc3146c5a5878e37435eb7f2
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-03-13 11:42:03 +0100

    Task02
    Upload

---
+++ 01.ADO.NET/02.VillainNames/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace _02.VillainNames
+{
+    public class StartUp
+    {
+        const string SqlConnectionString =
+            "Server=localhost;User Id = SA;Password = Qawsed12;Database=MinionsDB";
+
+        public static void Main(string[] args)
+        {
+            using var connection = new SqlConnection(SqlConnectionString);
+            connection.Open();
+
+            string query = @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                FROM Villains AS v 
+                                JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                              GROUP BY v.Id, v.Name 
+                                HAVING COUNT(mv.VillainId) > 3 
+                              ORDER BY COUNT(mv.VillainId)";
+
+            using (var command = new SqlCommand(query, connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var name = reader[0];
+                        var count = reader[1];
+
+                        Console.WriteLine($"{name} - {count}");
+                    }
+                }
+            }
+        }
+    }
+}
