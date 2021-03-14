4db5a7997e85427a7274bdb5a174ce67e8ab8f35
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-03-14 11:41:19 +0100

    Task05
    Upload

---
+++ 01.ADO.NET/05.ChangeTownNamesCasing/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+
+namespace _05.ChangeTownNamesCasing
+{
+    public class StartUp
+    {
+        const string SqlConnectionString =
+            "Server=localhost;User Id = SA;Password = Qawsed12;Database=MinionsDB";
+
+        public static void Main(string[] args)
+        {
+            using var connection = new SqlConnection(SqlConnectionString);
+            connection.Open();
+
+            string countryName = Console.ReadLine();
+
+            string updateTownNameQuery = @"UPDATE Towns
+                                              SET Name = UPPER(Name)
+                                            WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name = @countryName)";
+
+            string selectTownNamesQuery = @"SELECT t.Name 
+                                              FROM Towns as t
+                                              JOIN Countries AS c ON c.Id = t.CountryCode
+                                             WHERE c.Name = @countryName";
+
+            using var updateCommand = new SqlCommand(updateTownNameQuery, connection);
+            updateCommand.Parameters.AddWithValue("@countryName", countryName);
+            var affectedRows = updateCommand.ExecuteNonQuery();
+
+            if (affectedRows == 0)
+            {
+                Console.WriteLine("No town names were affected.");
+            }
+            else
+            {
+                Console.WriteLine($"{affectedRows} town names were affected.");
+
+                using var selectCommand = new SqlCommand(selectTownNamesQuery, connection);
+                selectCommand.Parameters.AddWithValue("@countryName", countryName);
+
+                using(var reader = selectCommand.ExecuteReader())
+                {
+                    var towns = new List<string>();
+                    while (reader.Read())
+                    {
+                        towns.Add((string)reader[0]);
+                    }
+                    Console.WriteLine($"[{string.Join(", ", towns)}]");
+                }
+            }
+        }
+    }
+}
