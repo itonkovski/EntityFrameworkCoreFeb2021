b0e7f69ca8f247b22180861a831cd2fafa087fcc
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-03-15 11:11:24 +0100

    Task07
    Upload

---
+++ 01.ADO.NET/07.PrintAllMinionNames/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+
+namespace _07.PrintAllMinionNames
+{
+    public class StartUp
+    {
+        const string SqlConnectionString =
+            "Server=localhost;User Id = SA;Password = Qawsed12;Database=MinionsDB";
+
+        public static void Main(string[] args)
+        {
+            using var connection = new SqlConnection(SqlConnectionString);
+            connection.Open();
+
+            var queryGetAllMinions = @"SELECT Name FROM Minions";
+            using var getAllMinionsCommand = new SqlCommand(queryGetAllMinions, connection);
+            using var reader = getAllMinionsCommand.ExecuteReader();
+
+            var listOfMinions = new List<string>();
+
+            while (reader.Read())
+            {
+                listOfMinions.Add(reader["Name"].ToString());
+            }
+
+            for (int i = 0; i < listOfMinions.Count / 2; i++)
+            {
+                Console.WriteLine(listOfMinions[i]);
+                Console.WriteLine(listOfMinions[listOfMinions.Count - i - 1]);
+            }
+
+            if (listOfMinions.Count % 2 != 0)
+            {
+                Console.WriteLine(listOfMinions[listOfMinions.Count / 2]);
+            }
+        }
+    }
+}
