ea517c36b0592684d393e0da4c02aace7a2a0dbf
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-03-16 11:50:13 +0100

    Task09
    Upload

---
+++ 01.ADO.NET/09.IncreaseAgeStoredProcedure/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace _09.IncreaseAgeStoredProcedure
+{
+    public class StartUp
+    {
+        const string SqlConnectionString =
+            "Server=localhost;User Id = SA;Password = Qawsed12;Database=MinionsDB";
+
+        public static void Main(string[] args)
+        {
+            using var connection = new SqlConnection(SqlConnectionString);
+            connection.Open();
+
+            int id = int.Parse(Console.ReadLine());
+
+            string query = "EXEC usp_GetOlder @id";
+            using var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.ExecuteNonQuery();
+
+            string selectQuery = @"SELECT Name, Age FROM Minions WHERE Id = @Id";
+            using var selectCommand = new SqlCommand(selectQuery, connection);
+            selectCommand.Parameters.AddWithValue("@id", id);
+            using var reader = selectCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader[0]} - {reader[1]} years old");
+            }
+        }
+    }
+}
