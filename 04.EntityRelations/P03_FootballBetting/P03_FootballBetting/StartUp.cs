e75cfd9ea4fe58c6c335872b862ee73e7cf8d3ed
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-03-19 10:39:24 +0100

    FootballBetting
    Upload

---
+++ 04.EntityRelations/P03_FootballBetting/P03_FootballBetting/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using P03_FootballBetting.Data;
+
+namespace P03_FootballBetting
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //var context = new FootballBettingContext();
+
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+        }
+    }
+}
